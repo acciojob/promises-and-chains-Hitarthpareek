9ec03939373cd3d5f5a266db8fac9c23f369f51c
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2025-11-26 11:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 const form = document.getElementById("myForm");
-const age = document.getElementById("age")
-const name = document.getElementById("name")
+const age = document.getElementById("age");
+const nameInput = document.getElementById("name");
 
 const checkAge = () => {
-	return new Promise((resolve,reject) =>{
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(age.value) > 18) {
+        resolve(`Welcome, ${nameInput.value}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${nameInput.value}. You aren't old enough.`);
+      }
+    }, 4000)
+  });
+};
 
-		setTimeout(()=> {
-					let mssg=""
-					if(age.value>18) resolve("Welcome, . You can vote.");
-					else reject("Oh sorry . You aren't old enough.");
-						},4000)
-	})
-}
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-	if(age.value.trim().length===0 || name.value.trim().length===0){
-		alert("Please enter valid details.")
-		return
-	}
-	checkAge().then((data)=>{
-		alert(data)
-		form.submit(); // success
-	}).catch((e)=>{
-		alert(e)
-		form.submit();
-	})
-	
-   
+  if (age.value.trim().length === 0 || nameInput.value.trim().length === 0) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  checkAge()
+    .then((data) => {
+      alert(data);
+      form.submit();
+    })
+    .catch((err) => {
+      alert(err);
+      form.submit();
+    });
 });
